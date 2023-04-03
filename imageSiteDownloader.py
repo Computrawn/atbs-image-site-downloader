0240dbf9b6ce2c622b964c5fa3b672f8c934dd1c
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-04-03 15:57:18 -0500

    Initial commit.

---
+++ imageSiteDownloader.py
@@ -0,0 +1,5 @@
+#! python3
+# imageSireDownloader.py — An exercise in web scraping.
+# For more information, see project_detail.txt.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys

05de71b89f6c9fce3015a4ccca8e18c3084b882e
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-04-08 16:45:05 -0500

    Added downloader function.

--- imageSiteDownloader.py
+++ imageSiteDownloader.py
@@ -1,31 +1,49 @@
 #! python3
 # imageSireDownloader.py — An exercise in web scraping.
 # For more information, see project_detail.txt.
-import re
+
+import concurrent.futures
 import requests
 import bs4
 
 img_list = []
+unsplash_name = []
 
 
-def find_source(search_object):  # Need to figure out how to find the src links.
+def find_source(search_object):
+    """Extract image source links using beautiful soup."""
     res = requests.get(search_object, timeout=60.0)
     res.raise_for_status()
     soup_file = bs4.BeautifulSoup(res.text, "lxml")
     image_container = soup_file.find_all(class_="MorZF")
     for image in image_container:
-        img_class = image.find_all("img")
-        img_list.append(img_class)
+        img_list.append(image.img["src"])
 
 
-def download_photos(img_list):  # Will change this eventually to download source links.
+def filter_links(img_list):
+    """Further filters source links to isolate urls and filenames."""
     for img in img_list:
-        print(img)
-        print("<-----*****----->")
+        img_id = img.split("/")[3]
+        img_match = img_id[:5]
+        if img_match == "photo":
+            img_name = img_id.split("?")
+            file_name = img_name[0]
+            unsplash_name.append(file_name)
+
+
+def link_downloader(unsplash):
+    for name in unsplash:
+        unsplash_link = f"https://images.unsplash.com/{name}"
+        unsplash_filename = f"{name}.jpg"
+        img_bytes = requests.get(unsplash_link, timeout=60.0).content
+        with open(unsplash_filename, "wb") as image_file:
+            image_file.write(img_bytes)
+            print(f"Downloading {unsplash_link} as {unsplash_filename}.")
 
 
 user_input = input("What would you like to search for? ")
 url = f"https://unsplash.com/s/photos/{user_input}"
 
 find_source(url)
-download_photos(img_list)
+filter_links(img_list)
+link_downloader(unsplash_name)

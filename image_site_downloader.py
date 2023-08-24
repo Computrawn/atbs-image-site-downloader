e87740608f1dc12674bb799f94d90581a4f253e6
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-24 17:20:47 -0500

    Convert filter_links function nested logic to list comprehension.

--- image_site_downloader.py
+++ image_site_downloader.py
@@ -4,64 +4,45 @@
 
 import concurrent.futures
 import requests
-import time
-import bs4
-
-img_list = []
-unsplash_name = []
+from bs4 import BeautifulSoup
 
 
 def find_source(search_object):
     """Extract image source links using beautiful soup."""
     res = requests.get(search_object, timeout=60.0)
     res.raise_for_status()
-    soup_file = bs4.BeautifulSoup(res.text, "lxml")
+    soup_file = BeautifulSoup(res.text, "lxml")
     image_container = soup_file.find_all(class_="MorZF")
-    for image in image_container:
-        img_list.append(image.img["src"])
+    img_list = [image.img["src"] for image in image_container]
+    return img_list
 
 
 def filter_links(img_list):
-    """Further filters source links to isolate urls and filenames."""
-    for img in img_list:
-        img_id = img.split("/")[3]
-        img_match = img_id[:5]
-        if img_match == "photo":
-            img_name = img_id.split("?")
-            file_name = img_name[0]
-            unsplash_name.append(file_name)
+    """Filters source links to isolate urls and filenames."""
+    unsplash_name = [
+        img.split("/")[3].split("?")[0]
+        for img in img_list
+        if img.split("/")[3][:5] == "photo"
+    ]
+    return unsplash_name
 
 
-# def nothread_downloader(unsplash):
-#     """Downloads files from list without threading."""
-#     for name in unsplash:
-#         unsplash_link = f"https://images.unsplash.com/{name}"
-#         unsplash_filename = f"{name}.jpg"
-#         img_bytes = requests.get(unsplash_link, timeout=60.0).content
-#         with open(f"photos/{unsplash_filename}", "wb") as image_file:
-#             image_file.write(img_bytes)
-
-
-def thread_downloader(unsplash):
-    """Downloads files from list with threading."""
+def downloader(unsplash):
+    """Downloads files from list."""
     unsplash_link = f"https://images.unsplash.com/{unsplash}"
-    unsplash_filename = f"{unsplash}.jpg"
     img_bytes = requests.get(unsplash_link, timeout=60.0).content
-    with open(f"photos/{unsplash_filename}", "wb") as image_file:
+    with open(f"photos/{unsplash}.jpg", "wb") as image_file:
         image_file.write(img_bytes)
 
 
-user_input = input("What would you like to search for? ")
-url = f"https://unsplash.com/s/photos/{user_input}"
+def main():
+    image_links = find_source(
+        f"https://unsplash.com/s/photos/{input('What would you like to search for? ')}"
+    )
+    unsplash_name = filter_links(image_links)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(downloader, unsplash_name)
 
-find_source(url)
-filter_links(img_list)
 
-start = time.perf_counter()
-# nothread_downloader(unsplash_name)
-
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    executor.map(thread_downloader, unsplash_name)
-
-finish = time.perf_counter()
-print(f"Downloaded all files in {finish - start} seconds.")
+if __name__ == "__main__":
+    main()
